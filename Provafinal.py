d0bb9c2b083692cd8b107e9b051a56394d6b42e1
Bruno Mata <bmfmata@gmail.com>
2018-07-07 16:19:26 -0300

    k

--- Provafinal.py
+++ Provafinal.py
@@ -166,7 +166,7 @@ while True:
 
 		if botao_valor == 0:
 			if vtemp > 10:
-				if aux_led_liga and aux_rele_liga == 0:
+				if aux_led_liga or aux_rele_liga == 0:
 					Aut_Liga()
 				time.sleep(10)
 				aux_led_liga = gpio.digital_read(LED)
