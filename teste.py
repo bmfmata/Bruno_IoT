dabad3c521aae21ae4b5ccd64605c2e3300ee9bd
Bruno Mata <bmfmata@gmail.com>
2018-07-11 01:16:02 -0300

    teste

--- teste.py
+++ teste.py
@@ -6,11 +6,12 @@ from libsoc import gpio
 from gpio_96boards import GPIO
 
 GPIO_CS = GPIO.gpio_id('GPIO_CS')
-LED = GPIO.gpio_id('GPIO_A')
-RELE = GPIO.gpio_id('GPIO_C')
+RELE = GPIO.gpio_id('GPIO_A')
+LED = GPIO.gpio_id('GPIO_C')
 BOTAO = GPIO.gpio_id('GPIO_E')
+TILT = GPIO.gpio_id('GPIO_G')
 
-pins = ((GPIO_CS, 'out'), (RELE, 'out'), (BOTAO, 'in'), (LED, 'out'),)
+pins = ((GPIO_CS, 'out'), (RELE, 'out'), (TILT, 'in') , (BOTAO, 'in'), (LED, 'out'),)
 
 spi = spidev.SpiDev()
 spi.open(0,0)
@@ -19,6 +20,13 @@ spi.mode = 0b00
 spi.bits_per_word = 8
 
 dweet = Dweet()
+
+alarme_bebe = 0
+bam_nuvem = 0
+ld_nuvem = 0
+reset_nuvem = 0
+estado_am = 0
+
 
 def readtemp(gpio):
 
@@ -47,40 +55,88 @@ def readLumi(gpio):
 
 	return  adcout
 
+
+#def Leitura_nuvem():
+#	global bam_nuvem, ld_nuvem, reset_nuvem
+#	resposta = dweet.latest_dweet(name="bmfmata")
+#	bam_nuvem = resposta['with'][0]['content']['bam_nuvem']
+#	ld_nuvem = resposta['with'][0]['content']['liga_des']
+#	reset_nuvem = resposta['with'][0]['content']['reset']
+
 	
 def liga():
-
+	
 	gpio.digital_write(LED, GPIO.HIGH)
 	gpio.digital_write(RELE, GPIO.HIGH)
 
 	
 def desliga():
 
+
 	gpio.digital_write(LED, GPIO.LOW)
 	gpio.digital_write(RELE, GPIO.LOW)
 
 
+def Aut_Liga():
+
+	liga()
+	alarme = 1
+	dweet.dweet_by_name(name="bmfmata", data={"alarme":alarme, "temp":vtemp, "lumi":vlumi, "bam_nuvem":bam_nuvem, "bebe":alarme_bebe, "reset":reset_nuvem, "estado": estado_am, "liga_des":ld_nuvem,})
+	print "Sistema Automatico! \n"		
+	print "Ar Condicionado Ligado"		
+	print ("Temperatura: %2.1f" %vtemp)
+	print ("Luminosidade: %2.1f \n" %vlumi)
+	
+
+def Aut_Des():
+
+	desliga()	
+	alarme = 0
+	dweet.dweet_by_name(name="bmfmata", data={"alarme":alarme, "temp":vtemp, "lumi":vlumi, "bam_nuvem":bam_nuvem, "bebe":alarme_bebe, "reset":reset_nuvem, "estado": estado_am, "liga_des":ld_nuvem,})
+	print "Sistema Automatico! \n"				
+	print "Ar Condicionado Desligado"		
+	print ("Temperatura: %2.1f" %vtemp)
+	print ("Luminosidade: %2.1f \n" %vlumi)
+	
+
+def Man_Liga():
+
+	liga()
+	alarme = 1
+	dweet.dweet_by_name(name="bmfmata", data={"alarme":alarme, "temp":vtemp, "lumi":vlumi, "bam_nuvem":bam_nuvem, "bebe":alarme_bebe, "reset":reset_nuvem, "estado": estado_am, "liga_des":ld_nuvem,})
+	print "Ar Condicionado Ligado"		
+	print ("Temperatura: %2.1f" %vtemp)
+	print ("Luminosidade: %2.1f \n" %vlumi)
+
+def Man_Des():
+
+	desliga()	
+	alarme = 0
+	dweet.dweet_by_name(name="bmfmata", data={"alarme":alarme, "temp":vtemp, "lumi":vlumi, "bam_nuvem":bam_nuvem, "bebe":alarme_bebe, "reset":reset_nuvem, "estado": estado_am, "liga_des":ld_nuvem,})
+	print "Ar Condicionado Desligado"		
+	print ("Temperatura: %2.1f" %vtemp)
+	print ("Luminosidade: %2.1f \n" %vlumi)
+
 
 while True:
 	with GPIO(pins) as gpio:
-		liga()
-		botao_valor = gpio.digital_read(BOTAO)		
+		resposta = dweet.latest_dweet(name="bmfmata")
+		bam_nuvem = resposta['with'][0]['content']['bam_nuvem']
+		botao_valor = gpio.digital_read(BOTAO)
 		vtemp = readtemp(gpio)
 		vlumi = readLumi(gpio)
-		alarme = 1
-		time.sleep(0.5)	
-		led_valor = gpio.digital_read(LED)
-		rele_valor = gpio.digital_read(RELE)
-		print ("Alarme: %d" %alarme)
-		print ("Led %d" %led_valor)	
-		print ("rele: %d" %rele_valor)
-		print ("botao: %d" %botao_valor)		
-		print ("Temperatura: %2.1f" %vtemp)
-		print ("Luminosidade: %2.1f" %vlumi)
-		dweet.dweet_by_name(name="bm_temp", data={"temp":vtemp})
-		dweet.dweet_by_name(name="bm_temp", data={"temp":vlumi})
-		resposta = dweet.latest_dweet(name="bm_temp")
-		time.sleep(2)
+		if botao_valor == 0:
+			estado_am = 1
+			if vtemp > 10:
+				Aut_Liga()
+			else:
+				Aut_Des()		
+		else:
+			print "Sistema Manual \n"
+		
+		time.sleep(10)
+		
+		
 
 
 
