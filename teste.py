676a365e801fc2ddbcd5a5885587c4b960176629
Bruno Mata <bmfmata@gmail.com>
2018-07-14 11:34:37 -0300

    k

--- teste.py
+++ teste.py
@@ -145,15 +145,20 @@ with GPIO(pins) as gpio:
 		resposta = dweet.latest_dweet(name="bmfmata")
 		bam_nuvem = resposta['with'][0]['content']['bam_nuvem']
 		botao_valor = gpio.digital_read(BOTAO)
-		if vlumi> 150:
+		rele = gpio.digital_read(RELE)
+		if vlumi > 100:
 			digital[0]=1
 			digital[1]=1
-			vtemp = vtemp + 140
+			
 		else:
 			digital[0]=0
 			digital[1]=0
-			vtemp = vtemp		
+			
 		writeDigital(gpio, digital)
+		#if rele == 1:
+		#	vtemp = vtemp + 140
+		#else
+		#	vtemp = vtemp
 		dweet.dweet_by_name(name="bmfmata", data={"bam_nuvem":bam_nuvem})
 		print ("Temperatura: %2.1f" %vtemp)
 		print ("Luminosidade: %2.1f \n" %vlumi)	
